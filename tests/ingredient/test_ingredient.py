1e585a2459291d82c93bad491fc8297e2abba6b2
RichardPMachado <richardmachado2211@gmail.com>
2023-06-19 23:13:22 -0300

    feat[requisito 1]: test igredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,8 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    fish = Ingredient("salmão")
+    fish2 = Ingredient("salmão")
+    assert hash(fish) == hash('carne')
+    assert fish == fish2

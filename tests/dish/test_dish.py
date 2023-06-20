aee144be73ad50f889e39e742a8e8ac596524841
RichardPMachado <richardmachado2211@gmail.com>
2023-06-20 19:52:06 -0300

    feat[requisito 2]: 7/12

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -5,18 +5,21 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 2
 def test_dish():
-    dish = Dish("salmão com salada", 80)
+    dish = Dish("salmão com salada", 80.00)
     assert dish.name == "salmão com salada"
-    assert dish.price == 80
+    assert dish.price == 80.00
     dish.add_ingredient_dependency('salmão', 2)
-    print(dish.get_ingredients())
     assert dish.get_ingredients() == {'salmão'}
+    print(dish.__hash__())
+    assert dish.__hash__() == hash(Dish("salmão com salada", 80.00))
 
     with pytest.raises(
         ValueError, match='Dish price must be greater then zero.'
     ):
         Dish("doce de coco", -1)
+
     with pytest.raises(
         TypeError, match='Dish price must be float.'
     ):
         Dish("doce de coco", '2')
+

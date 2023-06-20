7c8087c923becdd4fa976cbbb4e92575df0b4cc3
RichardPMachado <richardmachado2211@gmail.com>
2023-06-20 20:00:37 -0300

    feat[requisito 2]: 10/12 repr equal

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -10,7 +10,8 @@ def test_dish():
     assert dish.price == 80.00
     dish.add_ingredient_dependency('salmão', 2)
     assert dish.get_ingredients() == {'salmão'}
-    print(dish.__hash__())
+    print(dish.__repr__())
+    assert dish.__repr__() == "Dish(salmão com salada, 80.00)"
     assert dish.__hash__() == hash(Dish("salmão com salada", 80.00))
     assert dish.__hash__() != hash(Dish("salmão com salada", 70.00))
 

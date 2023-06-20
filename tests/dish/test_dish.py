88e941b73d61fa4cd84a6c0468419baf32e8255d
RichardPMachado <richardmachado2211@gmail.com>
2023-06-20 18:34:20 -0300

    feat[requisito 2]: 10/12

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,10 @@
+# import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
 # Req 2
 def test_dish():
-    pass
+    dish = Dish("salmão com salada", 10)
+    assert dish.name == "salmão com salada"
+    assert dish.price == 'R$ 80,00'
